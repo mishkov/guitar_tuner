60b82e372032a6038a913f1cf9cd7a4107e82785
Nikita Mishkov <mishkov.dd@gmail.com>
2025-08-27 21:56:12 +0300

    Increase targetSdk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
